6de0fab998cccb7e10a53b116bc209646e4fae0d
idkwhy-cyber <vicanyuru@gmail.com>
2024-03-06 09:03:31 +0100

    new file:   MultiplyNumbers/MultiplyNumbers.cs 	new file:   MultiplyNumbers/MultiplyNumbers.csproj

---
+++ MultiplyNumbers/MultiplyNumbers.cs
@@ -0,0 +1,18 @@
+﻿public class MultiplyNumbers
+{
+    public static void Main()
+    {
+        Console.WriteLine("This application will multiply three numbers for you !");
+        Console.WriteLine("input your first number ");
+        int n1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Input your second number ");
+        int n2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Input your third number ");
+        int n3 = Convert.ToInt32(Console.ReadLine());
+
+        int product = n1 * n2 * n3;
+
+        Console.WriteLine(n1 + " x " + n2 + " x " + n3 + " = " + n1*n2*n3);
+
+    }
+}
