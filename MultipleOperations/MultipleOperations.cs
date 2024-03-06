9bf8e9700490557c31a8caccea10cd2e2d598a3a
idkwhy-cyber <vicanyuru@gmail.com>
2024-03-06 10:26:28 +0100

    new file:   MultipleOperations/MultipleOperations.cs 	new file:   MultipleOperations/MultipleOperations.csproj

---
+++ MultipleOperations/MultipleOperations.cs
@@ -0,0 +1,16 @@
+﻿public class MultipleOperations
+{
+    public static void Main()
+    {
+        Console.WriteLine("Input your first number ");
+        int n1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Input your second number ");
+        int n2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(n1 + " + " + n2 + " = " + (n1+n2));
+        Console.WriteLine(n1 + " - " + n2 + " = " + (n1-n2) );
+        Console.WriteLine(n1 + " x " + n2 + " = " + (n1*n2));
+        Console.WriteLine(n1 + " / " + n2 + " = " + (n1/n2));
+        Console.WriteLine(n1 + " Mod " + n2 + " = " + (n1%n2));  
+
+    }
+}
