26b03171522b14e8c76834e942b921db2a056558
idkwhy-cyber <vicanyuru@gmail.com>
2024-03-06 14:25:19 +0100

    new file:   CheckDivisible3_7/CheckDivisible3_7.csproj 	new file:   CheckDivisible3_7/Program.cs

---
+++ CheckDivisible3_7/Program.cs
@@ -0,0 +1,18 @@
+﻿public class CheckDivisible
+{
+    public static void Main()
+    {
+        bool isDivisible;
+        Console.WriteLine("input your number");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        if (num1%3 == 0 || num1%7 == 0)
+        {
+            isDivisible = true;
+        }
+        else 
+        {
+            isDivisible = false;
+        }
+        Console.WriteLine(isDivisible);
+    }
+}
