a88b832afd1a540160aadbfffcc731ca7c5c199d
idkwhy-cyber <vicanyuru@gmail.com>
2024-03-06 14:02:48 +0100

    new file:   String4Copies/Program.cs 	new file:   String4Copies/String4Copies.csproj

---
+++ String4Copies/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class String4Copies
+{
+    public static void Main()
+    {
+        Console.WriteLine("Input your string here");
+        string? inputString = Console.ReadLine();
+        if (inputString.Length >= 4)
+        {
+            string? outputString = inputString.Substring((inputString.Length-4), 4);
+            Console.WriteLine(outputString + outputString + outputString + outputString);
+        }
+        else 
+        {
+            Console.WriteLine(inputString + inputString + inputString + inputString);
+        }
+
+    }
+}
