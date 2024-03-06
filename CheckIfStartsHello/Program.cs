089dcc2d2da1237f07a39bd2e528a1d8350b7242
idkwhy-cyber <vicanyuru@gmail.com>
2024-03-06 14:53:27 +0100

    new file:   CheckIfStartsHello/CheckIfStartsHello.csproj 	new file:   CheckIfStartsHello/Program.cs

---
+++ CheckIfStartsHello/Program.cs
@@ -0,0 +1,10 @@
+﻿public class CheckStartHello
+{
+    public static void Main()
+    {   
+        Console.WriteLine("Input Sentence: ");
+        string str1 = Console.ReadLine();
+        Console.WriteLine(str1.StartsWith("Hello"));
+
+    }
+}
