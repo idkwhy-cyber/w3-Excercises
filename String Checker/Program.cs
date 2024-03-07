07c2cc4d9e14274e384f37355613ca92008127b2
idkwhy-cyber <vicanyuru@gmail.com>
2024-03-07 11:32:37 +0100

    modified:   String Checker/Program.cs

--- String Checker/Program.cs
+++ String Checker/Program.cs
@@ -4,9 +4,16 @@
     {
         Console.WriteLine("Input String here: ");
         string str1 = Console.ReadLine();
-        string result = str1.Remove(str1.IndexOf("HP"), 2);
+        string result;
+        if (str1.IndexOf("HP") == 1)
+        {
+            result = str1.Remove(str1.IndexOf("HP"), 2);
+        }
+        else
+        {
+            result = str1;
+        }
         Console.WriteLine(result);
-
 
     }
 }
